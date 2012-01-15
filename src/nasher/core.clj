d03807f310f6f45d2d67e3d6573623747b621a1e
J. McConnell <j@ubermenschconsulting.com>
2012-01-15 13:01:23 -0500

    Initial import

---
+++ src/nasher/core.clj
@@ -0,0 +1,1 @@
+(ns nasher.core)
---
+++ test/nasher/test/core.clj
@@ -0,0 +1,6 @@
+(ns nasher.test.core
+  (:use [nasher.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
