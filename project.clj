ffceead384e4bebf8cd8800b83685058285b9127
J. McConnell <j@ubermenschconsulting.com>
2012-01-23 15:30:25 -0700

    Added a project description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject nasher "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A library for solving and analyzing strategic games."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.combinatorics "0.0.1"]])
