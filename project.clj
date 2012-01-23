9f3d1a80e1fe09a03a3c96598580b6966dc8992c
J. McConnell <j@ubermenschconsulting.com>
2012-01-23 15:32:56 -0700

    Dropped version number to alpha status

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nasher "1.0.0-SNAPSHOT"
+(defproject nasher "0.0.1-SNAPSHOT"
   :description "A library for solving and analyzing strategic games."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.combinatorics "0.0.1"]])
